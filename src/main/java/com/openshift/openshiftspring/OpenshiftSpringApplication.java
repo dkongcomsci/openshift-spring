434abc262a53f5b72b98ee22b69f4874ea19a762
kittaphart choichangthong <dkongcomsci@gmail.com>
2018-07-05 14:40:27 +0700

    Initial commit

---
+++ src/main/java/com/openshift/openshiftspring/OpenshiftSpringApplication.java
@@ -0,0 +1,12 @@
+package com.openshift.openshiftspring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OpenshiftSpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OpenshiftSpringApplication.class, args);
+	}
+}
